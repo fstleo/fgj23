544381b7f4dfbe92beec0156f6e20ee6ae3dcb20
Lev Bespalov <lev.bespalov@sulake.com>
2023-02-04 11:31:56 +0200

    Root grow

--- Assets/Root.cs
+++ Assets/Root.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class KickCube : MonoBehaviour
+public class Root : MonoBehaviour
 {
 
     [SerializeField]
@@ -12,15 +9,21 @@ public class KickCube : MonoBehaviour
     [SerializeField]
     private float _growSpeed;
 
+    [SerializeField]
+    private float _growAngle;
+
+    private Vector3 _direction = Vector3.down;
+
     public void Update()
     {
         var lastPosition = _rootLine.GetPosition(_rootLine.positionCount - 1);
         var lastIndex = _rootLine.positionCount - 1;
-        _rootLine.SetPosition(lastIndex, lastPosition + _growSpeed * Vector3.down * Time.deltaTime);
-        if ((_rootLine.GetPosition(lastIndex) - _rootLine.GetPosition(lastIndex - 1)).sqrMagnitude > 1)
+        _rootLine.SetPosition(lastIndex, lastPosition + _growSpeed * _direction * Time.deltaTime);
+        if ((_rootLine.GetPosition(lastIndex) - _rootLine.GetPosition(lastIndex - 1)).sqrMagnitude > 4)
         {
             _rootLine.positionCount ++;
             _rootLine.SetPosition(lastIndex + 1, _rootLine.GetPosition(lastIndex));
+            _direction = Quaternion.Euler(0, 0, Random.Range(-_growAngle, _growAngle)) * Vector3.down; 
         }
     }
 }
